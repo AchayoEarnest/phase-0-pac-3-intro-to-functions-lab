891f5d0da263fb58cce8d0a7f1188356cf9ecdb8
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-02 11:57:47 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log (string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log (string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string){
+    var one = "I can't hear you!";
+    var two = "YES INDEED!";
+    var three = "I would love to!";
+
+
+    if(string === string.toLowerCase(string)){
+        return one;
+    }else if(string === string.toUpperCase(string)){
+        return two;
+    }else if(string === "Let's have dinner together!") {
+        return three
+    }
+}
